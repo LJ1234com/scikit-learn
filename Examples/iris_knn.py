eff2e33ce19eddfa87dc6f60e50d43dcdc221069
LJ1234com <46641321+LJ1234com@users.noreply.github.com>
2019-01-27 10:51:18 +0800

    Create iris_knn.py

---
+++ Examples/iris_knn.py
@@ -0,0 +1,23 @@
+import numpy as np
+import sklearn
+import sklearn.datasets
+# import sklearn.neighbors.KNeighborsClassifier
+from sklearn.neighbors import KNeighborsClassifier
+
+
+iris = sklearn.datasets.load_iris()
+data = iris.data
+target = iris.target
+
+index = np.random.permutation(len(data))
+x_train = data[index[:-10]]
+y_train = target[index[:-10]]
+x_test = data[index[-10:]]
+y_test = target[index[-10:]]
+
+knn = sklearn.neighbors.KNeighborsClassifier()
+knn.fit(x_train, y_train)
+predict = knn.predict(x_test)
+
+print(predict)
+print(y_test)
